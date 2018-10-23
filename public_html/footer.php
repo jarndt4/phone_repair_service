4a6ea29f9a2585c0e407d5237b31c3d41a427390
Jack Arndt <jack.arndt@my.simpson.edu>
2018-10-23 09:00:11 -0500

    Include statement testing

---
+++ public_html/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+
+<footer>
+	<h3 style="font-size:12px;text-align:center">© 2018 Simp. CO. Phone Repair. Trademarks and brands are the property of their respective owners</h3>
+</footer>
---
+++ public_html/navigation.php
@@ -0,0 +1,34 @@
+<!-- Navigation Bar -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+ 	<a class="navbar-brand" href="#"><b>SimpCo Phone Company</b></a>
+ 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false	   " aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+ 	</button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="btn btn-primary" href="index.html" role="button">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="btn btn-primary" href="pricing.html" role="button">Pricing</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="btn btn-primary" href="schedule.html" role="button">Schedule</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="btn btn-primary" href="contact.html" role="button">Contact Us</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="btn btn-primary" href="locations.html" role="button">Our Locations</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="btn btn-primary" href="about.html" role="button">About Us</a>
+      </li>
+    </ul>
